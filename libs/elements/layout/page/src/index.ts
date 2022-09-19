ae69e80ce266dd5d674ed5e468190ee83927472e
Jente Rosseel <rosseeljente@gmail.com>
2022-09-19 17:43:58 +0200

    :truck: elements/layout/page & elements/layout/user-menu

---
+++ libs/elements/layout/page/src/index.ts
@@ -0,0 +1,5 @@
+export * from './lib/components/sidenav-container/nav-wrapper.component';
+export * from './lib/components/app-page/page.component';
+export * from './lib/components/app-navbar-side/navbar-side.component';
+
+export * from './lib/ital-page.module';
