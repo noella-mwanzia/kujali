0de562ba77681861115b7af91ca29008553f072c
Jente Rosseel <rosseeljente@gmail.com>
2022-09-19 17:36:06 +0200

    :wrench: :rocket: Configure app root

--- apps/kujali/src/app/app.module.ts
+++ apps/kujali/src/app/app.module.ts
@@ -1,11 +1,68 @@
 import { NgModule } from '@angular/core';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAnalyticsModule } from '@angular/fire/compat/analytics';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireFunctionsModule } from '@angular/fire/compat/functions';
+
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialBricksModule } from '@iote/bricks-angular';
+import { NgFireModule } from '@ngfi/angular';
+import { MultiLangModule } from '@ngfi/multi-lang';
+
+import { AuthorisationModule } from '@app/elements/base/authorisation';
+import { AppConfigurationModule } from '@app/elements/base/configuration';
+import { DateConfigurationModule } from '@app/elements/base/date-time';
+import { FirebaseConfigurationModule } from '@app/elements/base/firebase';
+
+import { UserStateModule } from '@app/state/user';
+// import { OrgStateModule } from '@app/state/organisation';
+
+
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule, BrowserAnimationsModule,
+
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule, AngularFirestoreModule, AngularFireFunctionsModule, NgFireModule,
+    AngularFireAnalyticsModule,
+    HttpClientModule,
+
+    MaterialBricksModule.forRoot(),
+
+    UserStateModule.forRoot(),
+    AuthorisationModule.forRoot(environment, environment.production),
+
+    AppConfigurationModule.forRoot(environment, environment.production),
+    DateConfigurationModule.forRoot(),
+    FirebaseConfigurationModule.forRoot(!environment.production, environment.useEmulators),
+    MultiLangModule.forRoot(true),
+    // UserNavModule,
+
+    MatProgressBarModule,
+
+    // AppConfigModule.forRoot(),
+
+    // DataModule.forRoot(),
+    // OrgStateModule.forRoot(),
+    // FlowsStateModule.forRoot(),
+    // ChatsStateModule.forRoot(),
+    // MessagingStateModule.forRoot(),
+    // CommChannelsStateModule.forRoot(),
+
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
