f00cbcb8b9d498beaa8ccb69b071d69a43c559f5
Jente Rosseel <rosseeljente@gmail.com>
2022-09-19 16:25:19 +0200

    ⚡ User state

---
+++ libs/state/user/src/lib/stores/user.store.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { UserService as UService } from '@ngfi/angular';
+import { Query } from '@ngfi/firestore-qbuilder';
+
+import { KuUser } from '@kujali/model/common/user';
+
+/**
+ * User Service for the scope of the iTalanta EcoSystem of Apps.
+ *
+ * @see @ngfi/angular/auth/UserService
+ */
+@Injectable({ providedIn: 'root' })
+export class UserStore extends UService<KuUser>
+{
+  override getUser(): Observable<KuUser>
+  {
+    const user = super.getUser();
+
+    return user.pipe(map(u => ((u && u.roles.access) ? u : null) as KuUser));
+  }
+
+  getUsers = () => this.getUsersBase(new Query().where('roles.access', '==', true));
+}
