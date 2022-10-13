4d1aa3a04063f2317655c86faccfeca0bc253782
Jente Rosseel <rosseeljente@gmail.com>
2022-10-13 14:50:57 +0200

    :truck: Move create budget modal

---
+++ libs/features/budgetting/budgets/src/lib/components/create-budget-modal/create-budget-modal.component.ts
@@ -0,0 +1,131 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+import { cloneDeep as ___cloneDeep, range as ___range } from 'lodash';
+
+import { Logger } from '@iote/bricks-angular';
+
+import { MONTHS, TimeTableGenerator, YEARS } from '@app/model/finance/planning/time';
+import { Budget, BudgetHeader, BudgetStatus } from '@app/model/finance/planning/budgets';
+
+import { BudgetsStore } from '@app/state/finance/budgetting/budgets';
+
+@Component({
+  selector: 'app-create-budget-modal',
+  templateUrl: './create-budget-modal.component.html',
+  styleUrls: ['./create-budget-modal.component.scss'],
+})
+/** Component responsible for budget creation. */
+export class CreateBudgetModalComponent implements OnInit
+{
+  // - SECTION "CREATE FORM"
+
+  /** If the budget has a child or note */
+  hasChild!: boolean;
+
+  /** Name of the budget */
+  budgetName!: string;
+  /** Start year of the budget */
+  startYear!: number;
+
+  // - ENDSECTION
+  // - SECTION "CREATE STUBS"
+
+  /** Years in which the budget is active. Prepopulated with available years on the platform. */
+  years = ___cloneDeep(YEARS);
+  
+  /** List of months of the budget. Prepopulated available months.
+   *      Every month is further prepopulated with an empty amount for budget structuring. */
+  months = ___cloneDeep(MONTHS).map((m, i) => { (<any>m).val = i; return m; });
+
+  inYears = ___range(1, 11).map((v) => ({ name: v, val: v /* * 12*/ }));
+  durationYear!: { name: number; val: number };
+  duration!: number;
+
+  // - ENDSECTION
+
+  constructor(
+      private _budgets$$: BudgetsStore,
+
+      @Inject(MAT_DIALOG_DATA) public childBudget: Budget | false,
+      public dialogRef: MatDialogRef<CreateBudgetModalComponent>,
+      private _logger: Logger) 
+  { }
+
+  ngOnInit = () => this.hasChild = !!this.childBudget;
+
+  onNoClick = () => this.dialogRef.close();
+
+  onDurationYearSet = () => this.duration = this.durationYear.val;
+  
+  /** 
+   * On form submit, this method creates the budget. 
+   * 
+   * Prepares the budget and adds it to the store.
+   */
+  createBudget(b: Budget) 
+  {
+    let overrideList     : string[] = [];
+    let overrideNameList : string[] = [];
+    const childrenList   : string[] = [];
+
+    if (this.childBudget) 
+    {
+      overrideList = (this.childBudget.overrideList.length > 0)
+                        ? this.childBudget.overrideList 
+                        : [];
+      overrideList.push(this.childBudget.id as string);
+      
+      overrideNameList = this.childBudget.overrideNameList.length > 0 
+                            ? this.childBudget.overrideNameList 
+                            : [];
+      overrideNameList.push(this.childBudget.name);
+    }
+
+    const budget = <Budget>{
+      name: this.budgetName,
+      startYear: this.startYear,
+      startMonth: 0,
+      duration: this.duration,
+
+      result: this._emptyBalance(),
+      status: BudgetStatus.Open,
+
+      overrideList,
+      overrideNameList,
+      childrenList,
+    };
+
+    // Add to store.
+    this._budgets$$.add(budget).subscribe((budget) => {
+      this._logger.log(
+        () => 'Budget ' + budget.name + ' with id ' + budget.id + ' created.'
+      );
+      this.dialogRef.close();
+    });
+  }
+
+  /** Generates a default result header for the budget. */
+  private _emptyBalance(): BudgetHeader 
+  {
+    const amountsYear = new TimeTableGenerator()
+      .getTimeFrameSkeleton(this.startYear, this.duration)
+      .map((y) => {
+        return {
+          year: y.year,
+          total: 0,
+          amountsMonth: y.months.map(() => {
+            return { amount: 0, units: 0, baseAmount: 0 };
+          }),
+        };
+      });
+
+    return {
+      isHeader: true,
+      type: 'result',
+      total: 0,
+      name: 'BUDGET.TABLE.DEFS.RESULT-HEADER',
+      amountsYear,
+    };
+  }
+}
---
+++ libs/features/budgetting/budgets/src/lib/financial-planning.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { FlexLayoutModule, MaterialBricksModule, MaterialDesignModule } from '@iote/bricks-angular';
+
+import { iTalPageModule } from '@app/elements/layout/page';
+
+// import { TransactionTypeManagementModule } from '../transaction-type-management/transaction-type-management.module';
+// import { FinancialPlanExplorerModule } from '../financial-plan-explorer/financial-plan-explorer.module';
+
+import { FinancialPlanningHomeComponent } from './components/financial-planning-home/financial-planning-home.component';
+
+import { SelectBudgetComponent } from './components/select-budget/select-budget.component';
+import { CreateBudgetModalComponent } from './components/create-budget-modal/create-budget-modal.component';
+
+import { BudgetService } from './services/budget.service';
+import { BudgetsOverviewService } from './services/budgets-overview.service';
+import { DisplayBudgetRecordComponent } from './components/display-budget-record/display-budget-record.component';
+import { ShareBudgetModalComponent } from './components/share-budget-modal/share-budget-modal.component';
+import { MultiLangModule } from '@ngfi/multi-lang';
+
+/**
+ * Financial-Planning module. Contains financial planning creation and forecasting.
+ */
+@NgModule({
+  imports: [CommonModule, MaterialBricksModule, MaterialDesignModule, FlexLayoutModule,
+            RouterModule, FormsModule,// CustomFormsModule,
+            
+            // TransactionTypeManagementModule, FinancialPlanExplorerModule,
+            
+            MultiLangModule,
+            iTalPageModule
+            // UserModule
+  ],
+
+  declarations: [FinancialPlanningHomeComponent, SelectBudgetComponent, DisplayBudgetRecordComponent, 
+                 CreateBudgetModalComponent, ShareBudgetModalComponent],
+
+  entryComponents: [CreateBudgetModalComponent, ShareBudgetModalComponent],
+
+  providers: [BudgetService, BudgetsOverviewService],
+  
+  exports: [FinancialPlanningHomeComponent],
+
+})
+export class FinancialPlanningModule { }
