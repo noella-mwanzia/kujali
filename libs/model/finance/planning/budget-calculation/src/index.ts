eb40bc74e165a35ce1b7dd0ed6ce4060d22759fc
Jente Rosseel <rosseeljente@gmail.com>
2022-10-29 18:16:22 +0300

    :package: Export tr-occ aggregation algo to state

--- libs/model/finance/planning/budget-calculation/src/index.ts
+++ libs/model/finance/planning/budget-calculation/src/index.ts
@@ -1,3 +1,3 @@
-
+export * from './lib/1_construct/construct-lines.function';
 export * from './lib/2_calculate/calc-local-budget.function';
 export * from './lib/3_aggregate/render-budget.function';
