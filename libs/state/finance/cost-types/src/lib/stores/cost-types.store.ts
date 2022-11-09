85d00aff0161eda62c2124963e9082ef29f1f19e
Jente Rosseel <rosseeljente@gmail.com>
2022-11-09 16:43:03 +0300

    :zap: Add ability to filter on type

--- libs/state/finance/cost-types/src/lib/stores/cost-types.store.ts
+++ libs/state/finance/cost-types/src/lib/stores/cost-types.store.ts
@@ -1,11 +1,11 @@
 import { Injectable } from "@angular/core";
-import { of } from "rxjs";
+import { filter, map, of } from "rxjs";
 
 import { Store } from "@iote/state";
 
 import { Logger } from "@iote/bricks-angular";
 
-import { DEFAULT_COST_CATEGORIES, LoadedTransactionTypeCategory } from "@app/model/finance/planning/budget-grouping";
+import { BudgetRowType, DEFAULT_COST_CATEGORIES, LoadedTransactionTypeCategory } from "@app/model/finance/planning/budget-grouping";
 
 
 /**
@@ -30,4 +30,7 @@ export class CostTypesStore extends Store<LoadedTransactionTypeCategory[]>
       });
   }
 
+  /** Get cost types of a specific master type (cost || income) */
+  getOfType = (type: BudgetRowType) => this.get()
+      .pipe(map(cs => cs.filter(c => c.type === type)));
 }
