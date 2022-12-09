7559c30c36a4a8d59cf20934735159f274d3ab08
ianodhiambo9 <ianodhiambo9@gmail.com>
2022-12-09 09:50:16 +0300

    :art: added an update form

--- libs/features/budgetting/budget-planning/src/lib/model/create-transaction-form.model.ts
+++ libs/features/budgetting/budget-planning/src/lib/model/create-transaction-form.model.ts
@@ -7,6 +7,7 @@ export function CreateTransactionFormGroup (_fb: FormBuilder, month: number): Fo
 
     // Section 1: Transaction
     pTNameFormGroup: _fb.group({
+      category: ['', Validators.required],
       type: ['', Validators.required],
       lineName: ['', Validators.required],
       budgetId: ['']
@@ -29,17 +30,80 @@ export function CreateTransactionFormGroup (_fb: FormBuilder, month: number): Fo
     // Section 4: determined by section 3 frequency
     pTIncreaseFormGroup: _fb.group({
       hasIncrease: [false],
+
+      amntIncrConfig: ['value'],
       amountIncreaseFrequency: [0],
       xTimesAmountIncreaseInterval: [0, Validators.required],
-
-      amntIncrConfig: [''],
       amountIncreaseRate: [0, Validators.required],
 
-      unitIncrConfig: [''],
+      unitIncrConfig: ['value'],
       unitIncreaseRate: [0, Validators.required],
       unitIncreaseFrequency: [0],
       xTimesUnitIncreaseInterval: [0],
     })
 
   })
+}
+
+export function CreateUpdateTransactionFormGroup (_fb: FormBuilder, plan: any ): FormGroup {
+  return _fb.group({
+    // Section 1: Transaction
+    pTNameFormGroup: _fb.group({
+      id: [''],
+      lineId: [plan.lineId],
+      category: [plan.category.id, Validators.required],
+      type: [plan.type.id, Validators.required],
+      lineName: [plan.lineName, Validators.required],
+      budgetId: [plan.budgetId]
+    }),
+
+    // Section 2: Value base
+    pTValueBaseFormGroup: _fb.group({
+      amount: [plan.amount, Validators.required],
+      units: [plan.units, Validators.required]
+    }),
+
+    // Section 3: Occurence
+    pTOccurenceFormGroup: _fb.group({
+      fromYear: [plan.fromYear, Validators.required],
+      fromMonth: [plan.fromMonth ?? MONTHS[0], Validators.required],
+      frequency: [GetBudgetFrequency(plan.frequency), Validators.required],
+      xTimesInterval: [plan.xTimesInterval]
+    }),
+
+    // Section 4: determined by section 3 frequency
+    pTIncreaseFormGroup: _fb.group({
+      hasIncrease: [plan.hasIncrease],
+
+      amntIncrConfig: [plan.amntIncrConfig.incrStyle],
+      amountIncreaseFrequency: [plan.amntIncrConfig.incrFreq],
+      xTimesAmountIncreaseInterval: [plan.amntIncrConfig.interval, Validators.required],
+      amountIncreaseRate: [plan.amntIncrConfig.incrRate, Validators.required],
+
+      unitIncrConfig: [plan.unitIncrConfig],
+      unitIncreaseRate: [plan.unitIncreaseRate, Validators.required],
+      unitIncreaseFrequency: [plan.unitIncreaseFrequency],
+      xTimesUnitIncreaseInterval: [plan.xTimesUnitIncreaseInterval],
+    })
+
+  })
+}
+
+function GetBudgetFrequency(frequency: number): string {
+  switch (frequency) {
+    case -1:
+      return 'Never';
+    case 0:
+      return 'Once';
+    case 1:
+      return 'Monthly';
+    case 90:
+      return 'Quarterly';
+    case 365:
+      return 'Yearly';
+    case 999:
+      return 'EveryXTimes';
+    default:
+      return ''
+  }
 }
