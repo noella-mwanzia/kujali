5ed3b80b39ba2229f7c1efd4cfe3e78d669897a7
ianodhiambo9 <ianodhiambo9@gmail.com>
2022-12-09 09:46:32 +0300

    :recycle: refactor of year focus

--- libs/features/budgetting/budget-explorer/src/lib/components/year-focus/year-focus.component.ts
+++ libs/features/budgetting/budget-explorer/src/lib/components/year-focus/year-focus.component.ts
@@ -1,20 +1,37 @@
 import { Component, Output, EventEmitter, Input } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
+import { BudgetRowType } from '@app/model/finance/planning/budget-grouping';
+import { MONTHS } from '@app/model/finance/planning/time';
+
+import { PlanTransactionModalComponent } from '@app/features/budgetting/budget-planning';
 
 @Component({
   selector: 'app-year-focus',
+  styleUrls: ['./year-focus.component.scss'],
   templateUrl: './year-focus.component.html'
 })
 /** 
  * Component on the side of the page which offers easy navigation between financial years.
  */
-export class YearFocusComponent
-{
+export class YearFocusComponent {
   @Input() title!: string;
+  @Input() budgetId: string;
+  @Input() type: BudgetRowType;
 
   @Output() navigateYearPressed = new EventEmitter<'prev' | 'next'>();
 
+  constructor(private dialog: MatDialog) { }
+
   navigateYear(nav: 'prev' | 'next') {
     this.navigateYearPressed.emit(nav);
-  } 
+  }
+
+  addCostOrIncome() {
+    this.dialog.open(PlanTransactionModalComponent,
+      {
+        data: { isInCreateMode: true, month: MONTHS[0], type: this.type, budgetId: this.budgetId },
+        minHeight: '600px'
+      })
+  }
 }
