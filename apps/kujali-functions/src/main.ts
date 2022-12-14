805dca3546183b6b8d44fde1da977ed8f94d981b
ianodhiambo9 <ianodhiambo9@gmail.com>
2022-12-14 12:33:56 +0300

    :fire: created wrong app

--- apps/kujali-functions/src/main.ts
+++
@@ -1,9 +0,0 @@
-
-import { config } from 'firebase-functions';
-import * as admin from 'firebase-admin';
-
-
-const conf = config()['firebase'];
-admin.initializeApp(conf);
-
-export * from './app/finance/budgeting/promote-budget.function';
