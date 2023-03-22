46d34509cfb1f1babb3b068b3eb6a8c4647abbd4
ianodhiambo9 <ianodhiambo9@gmail.com>
2023-03-22 10:04:56 +0300

    :package: create new bank account modal

--- libs/features/finance/banking/main/src/lib/banking-main.module.ts
+++ libs/features/finance/banking/main/src/lib/banking-main.module.ts
@@ -3,7 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { MaterialDesignModule, MaterialBricksModule, FlexLayoutModule } from '@iote/bricks-angular';
+import {
+  MaterialDesignModule,
+  MaterialBricksModule,
+  FlexLayoutModule,
+} from '@iote/bricks-angular';
 
 import { UserStateModule } from '@app/state/user';
 import { MultiLangModule } from '@ngfi/multi-lang';
@@ -12,21 +16,25 @@ import { iTalPageModule } from '@app/elements/layout/page';
 
 import { BankingPageComponent } from './pages/banking-page/banking-page.component';
 import { BankingRouterModule } from './banking.router';
+import { CreateNewBankAccountModalComponent } from './modals/create-new-bank-account-modal/create-new-bank-account-modal.component';
 
 @NgModule({
   imports: [
     CommonModule,
 
-    RouterModule, 
+    RouterModule,
     MultiLangModule,
-    MaterialDesignModule, MaterialBricksModule, FlexLayoutModule,
-    FormsModule, ReactiveFormsModule,
+    MaterialDesignModule,
+    MaterialBricksModule,
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule,
     UserStateModule,
 
     iTalPageModule,
 
-    BankingRouterModule
+    BankingRouterModule,
   ],
-  declarations: [BankingPageComponent]
+  declarations: [BankingPageComponent, CreateNewBankAccountModalComponent],
 })
 export class FinanceBankingModule {}
---
+++ libs/features/finance/banking/main/src/lib/modals/create-new-bank-account-modal/create-new-bank-account-modal.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-new-bank-account-modal',
+  templateUrl: './create-new-bank-account-modal.component.html',
+  styleUrls: ['./create-new-bank-account-modal.component.scss'],
+})
+export class CreateNewBankAccountModalComponent {}
