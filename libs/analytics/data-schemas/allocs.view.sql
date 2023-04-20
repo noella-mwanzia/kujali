aa61f43822e60d3cd96c7483d0d07161983627f2
AnnaL001 <43697008+AnnaL001@users.noreply.github.com>
2023-04-20 17:07:17 +0300

    :recycle: refactor: add sql for allocations view

---
+++ libs/analytics/data-schemas/allocs.view.sql
@@ -0,0 +1,14 @@
+SELECT
+  JSON_EXTRACT_SCALAR(path_params, '$.orgid') AS org_id,
+  
+  CAST(JSON_EXTRACT( data , '$.amount') as FLOAT64) AS amount,
+
+  JSON_EXTRACT_SCALAR(data, '$.invId') AS invoice_id,
+  JSON_EXTRACT_SCALAR(data, '$.pId') AS payment_id,
+
+  CAST(JSON_EXTRACT(data , '$.trType') as INT64) AS transaction_type,
+
+  JSON_EXTRACT_SCALAR(data, '$.createdBy') AS created_by,
+  TIMESTAMP_SECONDS(CAST(JSON_EXTRACT(data, '$.createdOn._seconds') as INT64)) AS created_on,
+
+FROM `project-kujali.kdev.kdev_allocs_raw_latest`
