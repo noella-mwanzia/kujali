f1c51eff1196b1d400b0a74d624d00e1d2c6fcbd
AnnaL001 <43697008+AnnaL001@users.noreply.github.com>
2023-04-27 15:32:27 +0300

    :recycle: refactor: update sql for expenses view

--- libs/analytics/data-schemas/expenses.view.sql
+++ libs/analytics/data-schemas/expenses.view.sql
@@ -1,21 +1,25 @@
 SELECT
   JSON_EXTRACT_SCALAR(path_params, '$.orgid') AS org_id,
+  document_id AS id,
   
   CAST(JSON_EXTRACT(data, '$.amount') as FLOAT64) AS amount,
-  CAST(JSON_EXTRACT(data, '$.baseAmount') as FLOAT64) AS base_amount,
+  JSON_EXTRACT_SCALAR(data, '$.allocatedTo') AS allocated_to,
 
-  CAST(JSON_EXTRACT_SCALAR(data, '$.id') as INT64) AS month,
-  CAST(JSON_EXTRACT_SCALAR(path_params, '$.year') as INT64) AS year,
+  JSON_EXTRACT_SCALAR(data, '$.note') AS note,
 
-  CAST(JSON_EXTRACT(data, '$.isOccurenceStart') as BOOL) AS is_occurrence_start,
-  JSON_EXTRACT_SCALAR(data, '$.occurenceId') AS occurrence_id, 
-  JSON_EXTRACT_SCALAR(data, '$.plan.id') AS plan_id,
-  JSON_EXTRACT_SCALAR(data, '$.plan.lineId') AS line_id,
-  JSON_EXTRACT_SCALAR(data, '$.plan.budgetId') AS budget_id,
+  JSON_EXTRACT_SCALAR(data, '$.planId') AS plan_id,
+  JSON_EXTRACT_SCALAR(data, '$.lineId') AS line_id,
+  JSON_EXTRACT_SCALAR(data, '$.budgetId') AS budget_id,
 
-  CAST(JSON_EXTRACT(data, '$.units') as INT64) AS units,
+  JSON_EXTRACT_SCALAR(data, '$.trCat') AS transaction_category,
+  JSON_EXTRACT_SCALAR(data, '$.trType') AS transaction_type,
+
+  CAST(JSON_EXTRACT(data, '$.vat') as INT64) AS vat,
+
+  TIMESTAMP_SECONDS(cast(JSON_EXTRACT(data, '$.date._seconds') as INT64)) AS date,
 
   JSON_EXTRACT_SCALAR(data, '$.createdBy') AS created_by,
   TIMESTAMP_SECONDS(cast(JSON_EXTRACT(data, '$.createdOn._seconds') as INT64)) AS created_on,
+  TIMESTAMP_SECONDS(cast(JSON_EXTRACT(data, '$.updatedOn._seconds') as INT64)) AS updated_on,
 
 FROM `project-kujali.kdev.kdev_expenses_raw_latest`
