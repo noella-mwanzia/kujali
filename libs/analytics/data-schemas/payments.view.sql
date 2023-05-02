8415878a65288ec7f446e26a450359d98c8a2a53
AnnaL001 <43697008+AnnaL001@users.noreply.github.com>
2023-05-02 11:04:32 +0300

    :recycle: refactor: update sql to include for_list_index column

--- libs/analytics/data-schemas/payments.view.sql
+++ libs/analytics/data-schemas/payments.view.sql
@@ -35,5 +35,7 @@ SELECT
   TIMESTAMP_SECONDS(cast(JSON_EXTRACT(data, '$.createdOn._seconds') as INT64)) AS created_on,
   TIMESTAMP_SECONDS(cast(JSON_EXTRACT(data, '$.updatedOn._seconds') as INT64)) AS updated_on,
 
+  offset AS for_list_index
+
 FROM `project-kujali.kdev.kdev_payments_raw_latest`,
-UNNEST(JSON_EXTRACT_ARRAY(data, '$.for')) AS for_list
+UNNEST(JSON_EXTRACT_ARRAY(data, '$.for')) AS for_list WITH OFFSET AS offset ORDER BY offset
