04e39de27458fcec66f1b975e61bd743b548c91d
AnnaL001 <43697008+AnnaL001@users.noreply.github.com>
2023-05-04 09:17:11 +0300

    :recycle: refactor: normalize companies table

--- libs/analytics/data-schemas/companies.view.sql
+++ libs/analytics/data-schemas/companies.view.sql
@@ -5,12 +5,8 @@ SELECT
   JSON_EXTRACT_SCALAR(data, '$.name') AS name,
   JSON_EXTRACT_SCALAR(data, '$.hq') AS headquarters,
   JSON_EXTRACT_SCALAR(data, '$.logoImgUrl') as logo_img_url,
-  JSON_EXTRACT_SCALAR(tags) AS tag,
 
   JSON_EXTRACT_SCALAR(data, '$.createdBy') AS created_by,
   TIMESTAMP_SECONDS(CAST(JSON_EXTRACT(data, '$.createdOn._seconds') as INT64)) AS created_on,
 
-  offset AS tag_index
-
-FROM `project-kujali.kdev.kdev_companies_raw_latest`,
-UNNEST(JSON_EXTRACT_ARRAY(data, '$.tags')) AS tags WITH OFFSET AS offset ORDER BY offset
+FROM `project-kujali.kdev.kdev_companies_raw_latest`
