3ffa20eaa9f99185c4e513a4f2cd0ba3a339b72e
AnnaL001 <43697008+AnnaL001@users.noreply.github.com>
2023-05-04 09:32:26 +0300

    :recycle: refactor: normalize payment allocs table

--- libs/analytics/data-schemas/payments_allocs.view.sql
+++ libs/analytics/data-schemas/payments_allocs.view.sql
@@ -5,18 +5,7 @@ SELECT
   CAST(JSON_EXTRACT(data, '$.credit') as FLOAT64) AS credit,
   CAST(JSON_EXTRACT(data, '$.allocStatus') as INT64) AS allocation_status,
 
-  JSON_EXTRACT_SCALAR(elements, '$.accId') AS account_id,
-  JSON_EXTRACT_SCALAR(elements, '$.accName') AS account_name,
-  CAST(JSON_EXTRACT(elements, '$.allocAmount') as FLOAT64) AS allocation_amount,
-  CAST(JSON_EXTRACT(elements, '$.allocMode') as INT64) AS allocation_mode,
-  JSON_EXTRACT_SCALAR(elements, '$.invoiceId') AS invoice_id,
-  JSON_EXTRACT_SCALAR(elements, '$.invoiceTitle') AS invoice_title,
-  JSON_EXTRACT_SCALAR(elements, '$.pId') AS payment_id,
-
   JSON_EXTRACT_SCALAR(data, '$.createdBy') AS created_by,
   TIMESTAMP_SECONDS(CAST(JSON_EXTRACT(data, '$.createdOn._seconds') as INT64)) AS created_on,
 
-  offset AS element_index
-
-FROM `project-kujali.kdev.kdev_payments_allocs_raw_latest`,
-UNNEST(JSON_EXTRACT_ARRAY(data, '$.elements')) AS elements WITH OFFSET AS offset ORDER BY offset
+FROM `project-kujali.kdev.kdev_payments_allocs_raw_latest`
