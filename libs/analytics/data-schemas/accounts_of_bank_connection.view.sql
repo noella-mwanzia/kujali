ef5e759ae9d48188df6147c93dfb888bcc7cad82
AnnaL001 <43697008+AnnaL001@users.noreply.github.com>
2023-05-04 09:09:41 +0300

    :recycle: refactor: normalize bank connections table

---
+++ libs/analytics/data-schemas/accounts_of_bank_connection.view.sql
@@ -0,0 +1,16 @@
+SELECT
+  CONCAT(document_id, '_a_', offset) as id,
+
+  JSON_EXTRACT_SCALAR(path_params, '$.orgid') AS org_id,
+  document_id AS connection_id,
+  JSON_EXTRACT_SCALAR(accounts, '$.sysAccId') AS sys_account_id,
+  JSON_EXTRACT_SCALAR(accounts, '$.bankAccId') AS bank_account_id,
+
+  CAST(JSON_EXTRACT(accounts, '$.active') as BOOL) AS account_active,
+  CAST(JSON_EXTRACT(accounts, '$.status') as INT64) AS account_status, 
+  CAST(JSON_EXTRACT(accounts, '$.type') as INT64) AS account_type, 
+
+  offset AS account_index
+
+FROM `project-kujali.kdev.kdev_bank_connections_raw_latest`,
+UNNEST(JSON_EXTRACT_ARRAY(data, '$.accounts')) AS accounts WITH OFFSET AS offset ORDER BY offset
---
+++ libs/analytics/data-schemas/ponto_bank_accounts.view.sql
@@ -0,0 +1,10 @@
+SELECT
+    JSON_EXTRACT_SCALAR(accounts, '$.bankAccId') AS bank_account_id,
+
+    JSON_EXTRACT_SCALAR(path_params, '$.orgid') AS org_id,
+
+    JSON_EXTRACT_SCALAR(accounts, '$.originalAccountInstance.attributes.holderName') AS account_holder,
+    JSON_EXTRACT_SCALAR(accounts, '$.originalAccountInstance.attributes.currency') AS account_currency,
+
+FROM `project-kujali.kdev.kdev_bank_connections_raw_latest`,
+UNNEST(JSON_EXTRACT_ARRAY(data, '$.accounts')) AS accounts WITH OFFSET AS offset ORDER BY offset
