d500b24f3ab5d1102c41fccf07cd143d8d108e00
ianodhiambo9 <ianodhiambo9@gmail.com>
2023-05-31 14:34:59 +0300

    :truck: contacts invoices

--- libs/features/finance/business/contacts/details/invoices/src/lib/contacts-invoices.module.ts
+++ libs/features/finance/business/contacts/details/invoices/src/lib/contacts-invoices.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MaterialDesignModule, MaterialBricksModule, FlexLayoutModule, MaterialFormBricksModule } from '@iote/bricks-angular';
+import { MultiLangModule } from '@ngfi/multi-lang';
+
+// import { AccessControlElementsModule } from '@app/elements/access-control';
+
+import { ContactInvoicesComponent } from './components/contact-invoices/contact-invoices.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    
+    MultiLangModule,
+    MaterialDesignModule,
+    MaterialBricksModule,
+    FlexLayoutModule,
+    MaterialFormBricksModule,
+
+    FormsModule,
+    ReactiveFormsModule,
+
+    // AccessControlElementsModule
+  ],
+  declarations: [
+    ContactInvoicesComponent
+  ],
+  exports: [
+    ContactInvoicesComponent
+  ]
 })
 export class ContactsInvoicesModule {}
