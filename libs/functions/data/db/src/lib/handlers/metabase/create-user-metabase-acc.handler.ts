7f40ec1a00de766a1b583e11bb8ab8a097453d1c
Noella <noellamwanzia@gmail.com>
2023-06-21 11:02:06 +0300

    feat: use metabase api to create new user on sign up

--- libs/functions/data/db/src/lib/handlers/metabase/create-user-metabase-acc.handler.ts
+++ libs/functions/data/db/src/lib/handlers/metabase/create-user-metabase-acc.handler.ts
@@ -1,14 +1,41 @@
-import { FunctionHandler, FunctionContext } from '@ngfi/functions';
+import * as request from 'request-promise';
 
+import { FunctionHandler, FunctionContext } from '@ngfi/functions';
 import { HandlerTools } from '@iote/cqrs';
 
-import * as jwt from 'jsonwebtoken';
+import { User } from '@iote/bricks';
 
-/** This handler creates a corresponding metabase account for new sign ups */
-export class CreateUserMetabaseAccHandler extends FunctionHandler<user:any, string>
+/* This handler creates a corresponding metabase account for new sign ups */
+export class CreateUserMetabaseAccHandler extends FunctionHandler<User, any>
 {
-  public async execute( data: any, context: FunctionContext, tools: HandlerTools)
+  public async execute( user: User, context: FunctionContext, tools: HandlerTools)
   {
-    
+    tools.Logger.log(() => `Creating metabase acc for User: ${user.uid}`);
+
+    request.post(this.createRequestConfig(user)).then();
   }
+
+  /** configure the contents of the api call */
+  createRequestConfig(user: User)
+  {
+    const displayname = user.displayName!.split('');
+
+    return {
+        url: 'https://elewa-group.metabaseapp.com/api/user',
+        headers: {
+          'Content-Type' : 'application/json',
+          'Accept': 'application/json',
+          'auth': {
+            'username': 'noella@elewa.ke',
+            'password': 'liMQAQAKa%2BZFkULYp',
+          }
+        },
+        body: {
+            'first_name': displayname[0],
+            'last_name': displayname[1],
+            'email': user.email
+        },
+        json: true
+      }
+}
 }
