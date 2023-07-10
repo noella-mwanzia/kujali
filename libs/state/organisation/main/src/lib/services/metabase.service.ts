87df79832561067d3f74c82c1b76ab605bbba19c
Noella <noellamwanzia@gmail.com>
2023-07-10 12:27:59 +0300

    ✨create service to retrieve metabase link.

---
+++ libs/state/organisation/main/src/lib/services/metabase.service.ts
@@ -0,0 +1,23 @@
+import { switchMap } from "rxjs";
+
+import { Injectable } from "@angular/core";
+import { AngularFireFunctions } from '@angular/fire/compat/functions';
+
+import { UserStore } from '@app/state/user';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class MetabaseService
+{
+  constructor( private _aFF: AngularFireFunctions,
+							 private authService: UserStore ){}
+
+	getMetabaseLink()
+	{
+		return this.authService.getUser().pipe(switchMap(user => this._aFF.httpsCallable('getMetabaseUrl')(user)));
+	}
+
+}
+
