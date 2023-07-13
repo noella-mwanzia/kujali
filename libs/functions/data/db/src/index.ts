b17acb91de6e888554128aeab04ba545ecde2465
Noella <noellamwanzia@gmail.com>
2023-07-13 12:19:44 +0300

    ❌delete unneeded handler

--- libs/functions/data/db/src/index.ts
+++ libs/functions/data/db/src/index.ts
@@ -1,3 +1,2 @@
 export * from './lib/handlers/create-payments.hander';
 export * from './lib/handlers/metabase/get-metabase-url.handler';
-export * from './lib/handlers/metabase/create-user-metabase-acc.handler';
